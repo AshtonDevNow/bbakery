c7e7a9331651423754aed9323be6bc4f40a41980
Ashton Bunn <ashtonwork@hotmail.com>
2022-05-19 09:05:43 +0800

    cupcake social media link working

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 
 
 // Class for intro
-// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-// tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
 // Class for transtions
-// window.onload = () => {
-//   const anchors = document.querySelectorAll('a');
-//   const transition_el = document.querySelector('.transition');
+window.onload = () => {
+  const anchors = document.querySelectorAll('a');
+  const transition_el = document.querySelector('.transition');
 
-//   setTimeout(() => {
-//     transition_el.classList.remove('is-active');
-//   }, 500);
+  setTimeout(() => {
+    transition_el.classList.remove('is-active');
+  }, 500);
 
-//   for (let i = 0; i < anchors.length; i++) {
-//     const anchor = anchors[i];
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
 
-//     anchor.addEventListener('click', e => {
-//       e.preventDefault();
-//       let target = e.target.href;
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.target.href;
 
-//       console.log(transition_el);
+      console.log(transition_el);
 
-//       transition_el.classList.add('is-active');
+      transition_el.classList.add('is-active');
 
-//       console.log(transition_el);
+      console.log(transition_el);
 
-//       setInterval(() => {
-//         window.location.href = target;
-//       }, 500);
-//     })
-//   }
-// }
+      setInterval(() => {
+        window.location.href = target;
+      }, 500);
+    })
+  }
+}
 
 
 
