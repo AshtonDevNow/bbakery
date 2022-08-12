d63a903f10ace26653d93cb83774581ae5d502e7
Ashton Bunn <ashtonwork@hotmail.com>
2022-08-12 09:21:07 +0800

    moved gsap to script js

---
+++ script.js
@@ -0,0 +1,92 @@
+// Class for intro
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+// Class for transtions
+window.onload = () => {
+  // Needed to comment the below and take out the page transition effect for the the a tag link
+  // to work for social media.
+  // const anchors = document.querySelectorAll('a');
+  const transition_el = document.querySelector(".transition");
+
+  setTimeout(() => {
+    transition_el.classList.remove("is-active");
+  }, 500);
+
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+
+    anchor.addEventListener("click", (e) => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      console.log(transition_el);
+
+      transition_el.classList.add("is-active");
+
+      console.log(transition_el);
+
+      setInterval(() => {
+        window.location.href = target;
+      }, 500);
+    });
+  }
+};
+
+// This is the JS for for the the menu pop-up
+
+// document.getElementById('colour-button').addEventListener('click',
+//   function() {
+//   document.querySelector('.bg-modal').style.display = 'flex';
+// });
+
+// Close button for pop-up
+
+// document.querySelectorAll('.close').addEventListener('click',
+//   function() {
+//     document.querySelectorAll('.bg-modal').style.display = 'none';
+// });
+
+// The new js code for the pop up modal
+
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const overlay = document.getElementById('overlay')
+
+// openModalButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = document.querySelector(button.dataset.modalTarget)
+//     openModal(modal)
+//   })
+// })
+
+// overlay.addEventListener('click', () => {
+//   const modals = document.querySelectorAll('.modal.active')
+//   modals.forEach(modal => {
+//     closeModal(modal)
+//   })
+// })
+
+// closeModalButtons.forEach(button => {
+//   button.addEventListener('click', () => {
+//     const modal = button.closest('.modal')
+//     closeModal(modal)
+//   })
+// })
+
+// function openModal(modal) {
+//   if (modal == null) return
+//   modal.classList.add('active')
+//   overlay.classList.add('active')
+// }
+
+// function closeModal(modal) {
+//   if (modal == null) return
+//   modal.classList.remove('active')
+//   overlay.classList.remove('active')
+// }
